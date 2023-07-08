7e16a8036cb6d852490a788a9e146509c039c052
vannvan <862670198@qq.com>
2023-07-09 00:54:34 +0800

    fix: update config

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,8 @@ const config = {
 
     // remove this if you don't want prerendering
     prerender: {
-      entries: ['*', '/sitemap.xml', '/rss.xml']
+      entries: ['*', '/sitemap.xml', '/rss.xml'],
+      handleMissingId: 'ignore'
     }
   }
 }
